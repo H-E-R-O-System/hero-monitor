678e0896b4073cebf7fff8f87b356c645188278a
“benhoskings1” <bensebandraf@gmail.com>
2024-02-20 13:42:25 +0000

    update button_test_2.py

--- button_test.py
+++ button_test.py
@@ -23,6 +23,7 @@ import gpiod
 
 button_dict = {2: "Hi", 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 10: "Vol_up"}
 # LED_PIN = 17
+
 chip = gpiod.Chip('gpiochip4')
 # led_line = chip.get_line(LED_PIN)
 button_lines = [chip.get_line(pin_num) for pin_num in button_dict.keys()]
