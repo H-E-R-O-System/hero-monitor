d29c3732462b56482e8ac4ebe832140dbbc74797
“benhoskings1” <bensebandraf@gmail.com>
2024-02-20 14:25:55 +0000

    update button_test_4.py

--- button_test.py
+++ button_test.py
@@ -19,25 +19,25 @@ import sys
 import RPi.GPIO as GPIO
 from time import sleep
 
-import gpiod
-
-# , 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 10: "Vol_up"
-button_dict = {2: "Hi", 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 4: "Vol_up"}
-# LED_PIN = 17
-
-chip = gpiod.Chip('gpiochip4')
-# led_line = chip.get_line(LED_PIN)
-button_lines = [chip.get_line(pin_num) for pin_num in range(5, 30)]
-# led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-for line in button_lines:
-    line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
-
-while True:
-    for idx, line in enumerate(button_lines):
-        button_state = line.get_value()
-        if button_state == 1:
-            print(idx)
-            # print(f"{list(button_dict.values())[idx]} Pressed")
+# import gpiod
+#
+# # , 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 10: "Vol_up"
+# button_dict = {2: "Hi", 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 4: "Vol_up"}
+# # LED_PIN = 17
+#
+# chip = gpiod.Chip('gpiochip4')
+# # led_line = chip.get_line(LED_PIN)
+# button_lines = [chip.get_line(pin_num) for pin_num in range(5, 30)]
+# # led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
+# for line in button_lines:
+#     line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
+#
+# while True:
+#     for idx, line in enumerate(button_lines):
+#         button_state = line.get_value()
+#         if button_state == 1:
+#             print(idx)
+#             # print(f"{list(button_dict.values())[idx]} Pressed")
 
 
 # import gpiod
@@ -61,3 +61,24 @@ while True:
 # finally:
 #    led_line.release()
 # button_line.release()
+
+
+
+from gpiozero import Button
+from signal import pause
+
+def say_hello(button):
+    print(button.pin)
+
+def say_goodbye():
+    print("Goodbye!")
+
+button1 = Button(27)
+button2 = Button(13)
+
+button1.when_pressed = say_hello
+button1.when_released = say_goodbye
+button2.when_pressed = say_hello
+button2.when_released = say_goodbye
+
+pause()
