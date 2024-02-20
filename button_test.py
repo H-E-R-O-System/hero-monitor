1cf46ae404059fd581066566ff5a247cf0a1cfd0
“benhoskings1” <bensebandraf@gmail.com>
2024-02-20 13:36:42 +0000

    update button_test_1.py

--- button_test.py
+++ button_test.py
@@ -21,18 +21,20 @@ from time import sleep
 
 button_dict = {2: "Hi", 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 10: "Vol_up"}
 
-GPIO.setmode(GPIO.BCM)
+import gpiod
+# LED_PIN = 17
+BUTTON_PIN = 3
+chip = gpiod.Chip('gpiochip4')
+# led_line = chip.get_line(LED_PIN)
+button_line = chip.get_line(BUTTON_PIN)
+# led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
+# button_line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
 
-for pin in button_dict.keys():
-    GPIO.setup(pin, GPIO.IN)
+while True:
+   button_state = button_line.get_value()
+   if button_state == 1:
+       print("Pressed")
+   # else:
+   #     led_line.set_value(0)
 
-running = True
-while running:
-    for button_id in button_dict.keys():
-        print(GPIO.input(button_id))
-        if GPIO.input(button_id) == 0 or GPIO.input(button_id) == 1:
-            print(f"Button pressed: {button_dict[button_id]}")
-            sleep(0.25)
-
-
-GPIO.cleanup()
+# button_line.release()
