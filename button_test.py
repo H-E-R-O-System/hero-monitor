5e1ffd574330b4c81b4543f711f5900fd17ce2f6
“benhoskings1” <bensebandraf@gmail.com>
2024-02-20 14:15:28 +0000

    update button_test_3.py

--- button_test.py
+++ button_test.py
@@ -27,7 +27,7 @@ button_dict = {2: "Hi", 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 4: "Vol_
 
 chip = gpiod.Chip('gpiochip4')
 # led_line = chip.get_line(LED_PIN)
-button_lines = [chip.get_line(pin_num) for pin_num in range(1, 5)]
+button_lines = [chip.get_line(pin_num) for pin_num in range(1, 10)]
 # led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 for line in button_lines:
     line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
@@ -36,7 +36,8 @@ while True:
     for idx, line in enumerate(button_lines):
         button_state = line.get_value()
         if button_state == 1:
-            print(f"{list(button_dict.values())[idx]} Pressed")
+            print(idx)
+            # print(f"{list(button_dict.values())[idx]} Pressed")
 
 
 # import gpiod
