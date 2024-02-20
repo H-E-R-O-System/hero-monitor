86a2a26ad023031db26e459e6d1d575877664192
“benhoskings1” <bensebandraf@gmail.com>
2024-02-20 13:42:14 +0000

    update button_test_1.py

--- button_test.py
+++ button_test.py
@@ -19,22 +19,19 @@ import sys
 import RPi.GPIO as GPIO
 from time import sleep
 
-button_dict = {2: "Hi", 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 10: "Vol_up"}
-
 import gpiod
+
+button_dict = {2: "Hi", 3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 10: "Vol_up"}
 # LED_PIN = 17
-BUTTON_PIN = 3
 chip = gpiod.Chip('gpiochip4')
 # led_line = chip.get_line(LED_PIN)
-button_line = chip.get_line(BUTTON_PIN)
+button_lines = [chip.get_line(pin_num) for pin_num in button_dict.keys()]
 # led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-button_line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
+for line in button_lines:
+    line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
 
 while True:
-   button_state = button_line.get_value()
-   if button_state == 0:
-       print("Pressed")
-   # else:
-   #     led_line.set_value(0)
-
-# button_line.release()
+    for line in button_lines:
+        button_state = line.get_value()
+        if button_state == 0:
+            print("Pressed")
