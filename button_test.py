4468a5473f82ec8ef51ea5ba63f982bdb50fecc9
“benhoskings1” <bensebandraf@gmail.com>
2024-02-20 13:19:18 +0000

    update button_test.py

--- button_test.py
+++ button_test.py
@@ -1,20 +1,31 @@
-import sys
-import RPi.GPIO as GPIO
-from time import sleep
+from gpiozero import Button
+from signal import pause
+def say_hello():
+    print("Hello!")
+def say_goodbye():
+    print("Goodbye!")
+button = Button(3)
+button.when_pressed = say_hello
+button.when_released = say_goodbye
+pause()
 
-button_dict = {3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 10: "Vol_up"}
+# import sys
+# import RPi.GPIO as GPIO
+# from time import sleep
+#
+# button_dict = {3: "Power", 5: "info", 7: "Home", 8: "Vol_don", 10: "Vol_up"}
+#
+# GPIO.setmode(GPIO.BCM)
+#
+# for pin in button_dict.keys():
+#     GPIO.setup(pin, GPIO.IN)
+#
+# running = True
+# while running:
+#     for button_id in button_dict.keys():
+#         if GPIO.input(button_id) == 0:
+#             print(f"Button pressed: {button_dict[button_id]}")
+#             sleep(0.25)
 
-GPIO.setmode(GPIO.BCM)
 
-for pin in button_dict.keys():
-    GPIO.setup(pin, GPIO.IN)
-
-running = True
-while running:
-    for button_id in button_dict.keys():
-        if GPIO.input(button_id) == 0:
-            print(f"Button pressed: {button_dict[button_id]}")
-            sleep(0.25)
-
-
-GPIO.cleanup()
+# GPIO.cleanup()
