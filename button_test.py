a3e61c5dd5b0e81a0261e34821117886b439085e
“benhoskings1” <bensebandraf@gmail.com>
2024-02-20 13:37:27 +0000

    update button_test_1.py

--- button_test.py
+++ button_test.py
@@ -28,7 +28,7 @@ chip = gpiod.Chip('gpiochip4')
 # led_line = chip.get_line(LED_PIN)
 button_line = chip.get_line(BUTTON_PIN)
 # led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-# button_line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
+button_line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
 
 while True:
    button_state = button_line.get_value()
