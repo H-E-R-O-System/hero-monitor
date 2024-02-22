cb535e814f61981f48cf61693f5b3743ba7b5312
“benhoskings1” <bensebandraf@gmail.com>
2024-02-22 15:54:46 +0000

    update button_test_6.py

--- button_test.py
+++ button_test.py
@@ -1,25 +1,25 @@
 import gpiod
 
 button_dict = {
-    4: "Power",
-    17: "info",
-    27: "Home",
-    22: "Vol_don",
-    23: "Vol_up"
+    4: "Power",     # Pin number 7
+    17: "info",     # Pin number 11
+    27: "Home",     # Pin number 13
+    22: "Vol_don",  # Pin number 15
+    23: "Vol_up"    # Pin number 16
 }
 # LED_PIN = 17
 
 chip = gpiod.Chip('gpiochip4')
 # led_line = chip.get_line(LED_PIN)
-button_lines = [chip.get_line(pin_num) for pin_num in range(5, 30)]
+button_lines = [(chip.get_line(pin_num), button_dict[pin_num]) for pin_num in button_dict.keys()]
 # led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-for line in button_lines:
+for (line, name) in button_lines:
     line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
 
 current_state = None
 
 while True:
-    for idx, line in enumerate(button_lines):
+    for idx, (line, name) in enumerate(button_lines):
         button_state = line.get_value()
         if button_state == 1:
-            print(f"{list(button_dict.values())[idx]} Pressed")
+            print(f"{name} Pressed")
