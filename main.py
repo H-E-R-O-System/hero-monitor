53a920ac5b92f863708511efe53c70d5b8858d30
Ben Hoskings <bensebandraf@gmail.com>
2024-03-01 16:45:50 +0000

    change button order

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from consult import Consultation
 os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
 pg.init()
 pg.event.pump()
-consult = Consultation(authenticate=True, seamless=True, auto_run=False)
+consult = Consultation(authenticate=False, seamless=True, auto_run=False)
 consult.loop()
 
