749bcb234e9e6f1bda2395f0b659db4423e752a9
Ben Hoskings <bensebandraf@gmail.com>
2024-03-02 21:30:16 +0000

    add camera test

--- camera_test.py
+++ camera_test.py
@@ -79,53 +79,70 @@
 #         consult_screen.update()
 #         base_screen.blit(consult_screen.touch_screen.surface, (0, 0))
 #         pg.display.update()
-def list_ports():
-    """
-    Test the ports and returns a tuple with the available ports
-    and the ones that are working.
-    """
-    is_working = True
-    dev_port = 0
-    working_ports = []
-    available_ports = []
-    while is_working:
-        camera = cv2.VideoCapture(dev_port)
-        if not camera.isOpened():
-            is_working = False
-            print("Port %s is not working." %dev_port)
-        else:
-            is_reading, img = camera.read()
-            w = camera.get(3)
-            h = camera.get(4)
-            if is_reading:
-                print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
-                working_ports.append(dev_port)
-            else:
-                print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
-                available_ports.append(dev_port)
-        dev_port +=1
 
-    return available_ports, working_ports
+import time
+from picamera2 import Picamera2, Preview
 
-import cv2
+picam = Picamera2()
 
-print(list_ports())
-image_size = (1280, 720)
-cap = cv2.VideoCapture(1)
-cap.set(3,image_size[0])
-cap.set(4,image_size[1])
+config = picam.create_preview_configuration()
+picam.configure(config)
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('affective.mp4', fourcc, 20.0, image_size)
+picam.start_preview(Preview.QTGL)
 
-while(True):
-    ret, frame = cap.read()
-    out.write(frame)
-    cv2.imshow('frame', frame)
-    c = cv2.waitKey(1)
-    if c & 0xFF == ord('q'):
-        break
+picam.start()
+time.sleep(2)
+picam.capture_file("test-python.jpg")
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+picam.close()
+
+# def list_ports():
+#     """
+#     Test the ports and returns a tuple with the available ports
+#     and the ones that are working.
+#     """
+#     is_working = True
+#     dev_port = 0
+#     working_ports = []
+#     available_ports = []
+#     while is_working:
+#         camera = cv2.VideoCapture(dev_port)
+#         if not camera.isOpened():
+#             is_working = False
+#             print("Port %s is not working." %dev_port)
+#         else:
+#             is_reading, img = camera.read()
+#             w = camera.get(3)
+#             h = camera.get(4)
+#             if is_reading:
+#                 print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+#                 working_ports.append(dev_port)
+#             else:
+#                 print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
+#                 available_ports.append(dev_port)
+#         dev_port +=1
+#
+#     return available_ports, working_ports
+#
+# import cv2
+#
+# print(list_ports())
+# image_size = (1280, 720)
+# cap = cv2.VideoCapture(0)
+# cap.set(3,image_size[0])
+# cap.set(4,image_size[1])
+#
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+# out = cv2.VideoWriter('affective.mp4', fourcc, 20.0, image_size)
+#
+# while(True):
+#     ret, frame = cap.read()
+#     out.write(frame)
+#     cv2.imshow('frame', frame)
+#     c = cv2.waitKey(1)
+#     if c & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# out.release()
+# cv2.destroyAllWindows()
