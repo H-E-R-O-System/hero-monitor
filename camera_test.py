87acf399a3b2b9d9a3577acb6b672ae7355c1bb1
Ben Hoskings <bensebandraf@gmail.com>
2024-03-03 01:03:37 +0000

    add dual test

--- camera_test.py
+++ camera_test.py
@@ -1,14 +1,15 @@
 import time
+from picamera2 import Picamera2, Preview
 
-from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
+picam = Picamera2()
 
-picam2 = Picamera2()
-video_config = picam2.create_video_configuration()
-picam2.configure(video_config)
+config = picam.create_preview_configuration()
+picam.configure(config)
+picam.start()
 
-encoder = H264Encoder(10000000)
+for i in range(1, 10):
+    picam.capture_file(f"ts{i}.jpg")
+    print(f"Captured image {i}")
+    time.sleep(3)
 
-picam2.start_recording(encoder, 'test.h264')
-time.sleep(10)
-picam2.stop_recording()
+picam.stop()
