28fd6a100f1d28cc40dc7add39728549d37fd6e5
Ben Hoskings <bensebandraf@gmail.com>
2024-03-03 00:41:28 +0000

    flip camera

--- camera_test.py
+++ camera_test.py
@@ -98,12 +98,13 @@
 # cv2.waitKey(0)
 
 
-import time
+import time, libcamera
 from picamera2 import Picamera2, Preview
 
 picam = Picamera2()
 
-config = picam.create_preview_configuration()
+config = picam.create_preview_configuration(main={"size": (1600, 1200)})
+config["transform"] = libcamera.Transform(hflip=1, vflip=1)
 picam.configure(config)
 
 picam.start_preview(Preview.QTGL)
