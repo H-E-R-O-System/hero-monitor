f1dc0c80e7a9cea59846545a253aac83e7c01036
Ben Hoskings <bensebandraf@gmail.com>
2024-03-03 00:35:42 +0000

    add camera test

--- camera_test.py
+++ camera_test.py
@@ -81,38 +81,38 @@
 #         pg.display.update()
 
 # import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-import cv2
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-# allow the camera to warmup
-time.sleep(0.1)
-# grab an image from the camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
-# display the image on screen and wait for a keypress
-cv2.imshow("Image", image)
-cv2.waitKey(0)
-
-
+# from picamera.array import PiRGBArray
+# from picamera import PiCamera
 # import time
-# from picamera2 import Picamera2, Preview
-#
-# picam = Picamera2()
-#
-# config = picam.create_preview_configuration()
-# picam.configure(config)
-#
-# picam.start_preview(Preview.QTGL)
-#
-# picam.start()
-# time.sleep(2)
-# picam.capture_file("test-python.jpg")
-#
-# picam.close()
+# import cv2
+# # initialize the camera and grab a reference to the raw camera capture
+# camera = PiCamera()
+# rawCapture = PiRGBArray(camera)
+# # allow the camera to warmup
+# time.sleep(0.1)
+# # grab an image from the camera
+# camera.capture(rawCapture, format="bgr")
+# image = rawCapture.array
+# # display the image on screen and wait for a keypress
+# cv2.imshow("Image", image)
+# cv2.waitKey(0)
+
+
+import time
+from picamera2 import Picamera2, Preview
+
+picam = Picamera2()
+
+config = picam.create_preview_configuration()
+picam.configure(config)
+
+picam.start_preview(Preview.QTGL)
+
+picam.start()
+time.sleep(2)
+picam.capture_file("test-python.jpg")
+
+picam.close()
 
 # def list_ports():
 #     """
