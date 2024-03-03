a7cb19014aff4fc9340e780417d861ed44950402
Ben Hoskings <bensebandraf@gmail.com>
2024-03-03 01:01:58 +0000

    add dual test

--- camera_audio_test.py
+++ camera_audio_test.py
@@ -5,13 +5,8 @@ from picamera2.encoders import H264Encoder
 import wave
 import pyaudio
 
-pyaud = pyaudio.PyAudio()
 
-device_info = pyaud.get_default_input_device_info()
-print(device_info)
-rate = device_info["defaultSampleRate"]
-chunk, max_time = 1024, 10
-path = "mic_test.wav"
+picam2.start_recording(encoder, 'affective_record.h264')
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
@@ -19,25 +14,37 @@ picam2.configure(video_config)
 
 encoder = H264Encoder(10000000)
 
-stream = pyaud.open(format=pyaudio.paInt16, channels=1, rate=int(rate), input=True, frames_per_buffer=chunk)
-picam2.start_recording(encoder, 'affective_record.h264')
-
-frames = []
-
-iter_per_second = int((rate / chunk))
-for i in range(0, iter_per_second * max_time):
-    data = stream.read(chunk)
-    # data is a raw bytes object
-    frames.append(data)
-
-stream.stop_stream()
-stream.close()
-
 picam2.stop_recording()
 
-wf = wave.open(path, 'wb')
-wf.setnchannels(1)
-wf.setsampwidth(pyaud.get_sample_size(pyaudio.paInt16))
-wf.setframerate(rate)
-wf.writeframes(b''.join(frames))
-wf.close()
+# pyaud = pyaudio.PyAudio()
+
+# device_info = pyaud.get_default_input_device_info()
+# print(device_info)
+# rate = device_info["defaultSampleRate"]
+# chunk, max_time = 1024, 10
+# path = "mic_test.wav"
+
+# stream = pyaud.open(format=pyaudio.paInt16, channels=1, rate=int(rate), input=True, frames_per_buffer=chunk)
+
+
+# frames = []
+#
+# iter_per_second = int((rate / chunk))
+# for i in range(0, iter_per_second * max_time):
+#     data = stream.read(chunk)
+#     # data is a raw bytes object
+#     frames.append(data)
+#
+# stream.stop_stream()
+# stream.close()
+
+
+
+
+
+# wf = wave.open(path, 'wb')
+# wf.setnchannels(1)
+# wf.setsampwidth(pyaud.get_sample_size(pyaudio.paInt16))
+# wf.setframerate(rate)
+# wf.writeframes(b''.join(frames))
+# wf.close()
