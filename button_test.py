7cb8bd7e7b43eb0224e5b771d91d75805d0364e9
“benhoskings1” <bensebandraf@gmail.com>
2024-04-17 13:26:21 +0100

    button updates

--- button_test.py
+++ button_test.py
@@ -1,25 +1,25 @@
 import gpiod
 
 # Define Raspberry Pi button pins
-button_dict = {
-    4: "Power",     # Pin number 7
-    17: "Home",     # Pin number 11
-    27: "Vol Down",     # Pin number 13
-    22: "Info",  # Pin number 15
-    23: "Vol Up"    # Pin number 16
-}
+# button_dict = {
+#     4: "Power",     # Pin number 7
+#     17: "Home",     # Pin number 11
+#     27: "Vol Down",     # Pin number 13
+#     22: "Info",  # Pin number 15
+#     23: "Vol Up"    # Pin number 16
+# }
 
 # LED_PIN = 17
 
-chip = gpiod.Chip('gpiochip4')
-# led_line = chip.get_line(LED_PIN)
-button_lines = [(chip.get_line(pin_num), button_dict[pin_num]) for pin_num in button_dict.keys()]
-# led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-
-for (line, name) in button_lines:
-    line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
-
-current_state = None
+# chip = gpiod.Chip('gpiochip4')
+# # led_line = chip.get_line(LED_PIN)
+# button_lines = [(chip.get_line(pin_num), button_dict[pin_num]) for pin_num in button_dict.keys()]
+# # led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
+#
+# for (line, name) in button_lines:
+#     line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
+#
+# current_state = None
 
 
 class Buttons:
